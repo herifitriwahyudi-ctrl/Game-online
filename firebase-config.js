c95e9157b0001b8048b9c0df143cf4c41b96a83e
herifitriwahyudi-ctrl <herifitriwahyudi@gmail.com>
2026-09-01 21:16:24 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,12 @@
-// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDyn8s6NtJqOnN_hvWEqoRYbvYbq7TQqO8",
   authDomain: "dragonhunter-2fdb7.firebaseapp.com",
+  databaseURL: "https://dragonhunter-2fdb7-default-rtdb.asia-southeast1.firebasedatabase.app", // <-- Tambahkan ini!
   projectId: "dragonhunter-2fdb7",
   storageBucket: "dragonhunter-2fdb7.firebasestorage.app",
   messagingSenderId: "138293020401",
   appId: "1:138293020401:web:90502b724d0c30aabf4b50"
 };
 
-// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+const db = firebase.database(); // Gunakan Realtime Database
